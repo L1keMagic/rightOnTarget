91de3e4eb12af8ca2d59bab733265eddb2c82210
L1keMagic <karachev.artur@gmail.com>
2021-08-23 15:49:51 +0300

    started creating extpa game -colors

---
+++ Right On Target/ColorGameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ColorGameViewController.swift
+//  Right On Target
+//
+//  Created by Артур Карачев on 23.08.2021.
+//
+
+import UIKit
+
+class ColorGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
